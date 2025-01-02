06082afd3e4b6e7da5410b874eacb5be94054f4c
Varsha <varsharam2003@gmail.com>
2025-01-02 14:38:54 +0530

    Changes made

--- src/main/java/com/example/automation/LoginAutomationTest.java
+++ src/main/java/com/example/automation/LoginAutomationTest.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LoginAutomationTest {
  @Test
  public void testLogin() {
